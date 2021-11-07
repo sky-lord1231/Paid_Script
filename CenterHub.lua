0736d71c1f557d5d9559c77ff9bf3c591f9fe9ed
sky-lord1231 <84723672+sky-lord1231@users.noreply.github.com>
2021-11-07 19:08:33 +0700

    Update CenterHub.lua

--- CenterHub.lua
+++ CenterHub.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("ปิด script 1 - 3 วัน \nScript Not Working 1 - 3 day\n"..os.date())
+game.Players.LocalPlayer:Kick("\nปิด script 1 - 3 วัน \nScript Not Working 1 - 3 day\n"..os.date())
